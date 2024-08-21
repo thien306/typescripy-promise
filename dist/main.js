14702b10439bb0385d200fff3d55d41c7cd8d4ab
thien306 <nguyenvantho306@gmail.com>
2024-08-21 08:58:55 +0700

    init commit

---
+++ dist/main.js
@@ -0,0 +1,20 @@
+let money = 10000;
+const buyACar = (car) => {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            if (money >= 10000) {
+                resolve("có thể mua " + car);
+            }
+            else {
+                reject("Không đủ tiền");
+            }
+        }, 100);
+    }));
+};
+money = 1000001;
+const promise = buyACar("xe vinfast").then(value => {
+    console.log(value);
+}, error => {
+    console.log(error);
+});
+//# sourceMappingURL=main.js.map
